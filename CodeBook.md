69130e69f8f0b308d1bd63ba8aaf4ff4bd268e4f
Anat-Kedem <anat-kedem@hotmail.com>
2015-02-15 23:06:50 +0200

    add CodeBook and run_analysis

---
+++ CodeBook.md
@@ -0,0 +1,151 @@
+#Code Book
+This Code Book describes the tidy data file "Mean_ActivityDataBySmartphones.txt", and refers to the original data.
+
+---
+##Source data
+---
+**Acknowlegement:** *Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012*
+
+
+###Download link:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+---
+##Results Data
+---
+###Description
+The resulted data is a tidy data of one table file named "Mean_ActivityDataBySmartphones.txt". The tidy data includes the average of each variable for each activity and each subject.
+
+The following table shows the frame of the tidy data (see rows and variables description below):
+
+Subject | Activity_Mean     | *variable1* | *variable2* |*(rest of variables)*
+------- | :-------------------- | :-----------: | :-----------: | :----------------:
+1       |Walking                |...          |...          |...
+1       |WalkingUpstairs      |...          |...          |...
+1       |WalkingDownstairs    |...          |...          |...
+1       |Sitting               |...          |...          |...
+1       |Standing              |...          |...          |...
+1       |Laying                |...          |...          |...
+1       |**Mean_of_Activities**|...          |...          |...
+2       |Walking               |...          |...          |...
+2       |WalkingUpstairs      |...          |...          |...
+
+Where (...) is the mean of the relevant data. For example, the value in row 1 column 3 is the mean of all measurements of activity "Walking" for *"variable1"* (see actual variable names below) for subject "1", and the value in row 7 column 3 is the mean of all measurements of all activities for *"variable1"* for subject "1".
+
+The full table includes 210 observation rows (beyond the one title row of the column names) and x variables. The total columns is 86 + 2 (variables + the subject code and activity name columns).
+
+####Rows
+The tidy data includes 210 observation rows. For each subject the tidy data includes 6 rows that contain means of six types of activity and one row contains the mean of all activities for this subject. 30 subjects multiple 7 rows = 210 rows.
+####Variables
+The tidy data includes 88 columns, from these 86 are variables and 2 are codes and names of the subjects and activities. From the original variables of the data (see the above link), the **mean and std variables** for the 3-axial signals in the X, Y and Z directions were used in the tidy data:
+
+**Selected Main Variables:**
+- mean()
+- std()
+- meanFreq()
+- gravityMean
+- tBodyAccMean
+- tBodyAccJerkMean
+- tBodyGyroMean
+- tBodyGyroJerkMean
+
+**The full names of the 86 variables and domain of the tidy data are listed below:**
+
+The domain for names with "t" as in "tBodyAcc-mean-X" is time, and the domain for names with "f" as in "fBodyAcc-mean-X" is frequency. Domain described instead of units since all variables were ormalized and bounded within [-1,1].
+
+Column | Variable Name 
+:----- | :------------ 
+[3] | "tBodyAcc-mean-X"
+[4] | "tBodyAcc-mean-Y"
+[5] | "tBodyAcc-mean-Z"
+[6] | "tBodyAcc-std-X"
+[7] | "tBodyAcc-std-Y"
+[8] | "tBodyAcc-std-Z"
+[9] | "tGravityAcc-mean-X"
+[10] | "tGravityAcc-mean-Y"
+[11] | "tGravityAcc-mean-Z"
+[12] | "tGravityAcc-std-X"
+[13] | "tGravityAcc-std-Y"
+[14] | "tGravityAcc-std-Z"
+[15] | "tBodyAccJerk-mean-X"
+[16] | "tBodyAccJerk-mean-Y"
+[17] | "tBodyAccJerk-mean-Z"
+[18] | "tBodyAccJerk-std-X"
+[19] | "tBodyAccJerk-std-Y"
+[20] | "tBodyAccJerk-std-Z"
+[21] | "tBodyGyro-mean-X"
+[22] | "tBodyGyro-mean-Y"
+[23] | "tBodyGyro-mean-Z"
+[24] | "tBodyGyro-std-X"
+[25] | "tBodyGyro-std-Y"
+[26] | "tBodyGyro-std-Z"
+[27] | "tBodyGyroJerk-mean-X"
+[28] | "tBodyGyroJerk-mean-Y"
+[29] | "tBodyGyroJerk-mean-Z"
+[30] | "tBodyGyroJerk-std-X"
+[31] | "tBodyGyroJerk-std-Y"
+[32] | "tBodyGyroJerk-std-Z"
+[33] | "tBodyAccMag-mean"
+[34] | "tBodyAccMag-std"
+[35] | "tGravityAccMag-mean"
+[36] | "tGravityAccMag-std"
+[37] | "tBodyAccJerkMag-mean"
+[38] | "tBodyAccJerkMag-std"
+[39] | "tBodyGyroMag-mean"
+[40] | "tBodyGyroMag-std"
+[41] | "tBodyGyroJerkMag-mean"
+[42] | "tBodyGyroJerkMag-std"
+[43] | "fBodyAcc-mean-X"
+[44] | "fBodyAcc-mean-Y"
+[45] | "fBodyAcc-mean-Z"
+[46] | "fBodyAcc-std-X"
+[47] | "fBodyAcc-std-Y"
+[48] | "fBodyAcc-std-Z"
+[49] | "fBodyAcc-meanFreq-X"
+[50] | "fBodyAcc-meanFreq-Y"
+[51] | "fBodyAcc-meanFreq-Z"
+[52] | "fBodyAccJerk-mean-X"
+[53] | "fBodyAccJerk-mean-Y"
+[54] | "fBodyAccJerk-mean-Z"
+[55] | "fBodyAccJerk-std-X"
+[56] | "fBodyAccJerk-std-Y"
+[57] | "fBodyAccJerk-std-Z"
+[58] | "fBodyAccJerk-meanFreq-X"
+[59] | "fBodyAccJerk-meanFreq-Y"
+[60] | "fBodyAccJerk-meanFreq-Z"
+[61] | "fBodyGyro-mean-X"
+[62] | "fBodyGyro-mean-Y"
+[63] | "fBodyGyro-mean-Z"
+[64] | "fBodyGyro-std-X"
+[65] | "fBodyGyro-std-Y"
+[66] | "fBodyGyro-std-Z"
+[67] | "fBodyGyro-meanFreq-X"
+[68] | "fBodyGyro-meanFreq-Y"
+[69] | "fBodyGyro-meanFreq-Z"
+[70] | "fBodyAccMag-mean"
+[71] | "fBodyAccMag-std"
+[72] | "fBodyAccMag-meanFreq"
+[73] | "fBodyBodyAccJerkMag-mean"
+[74] | "fBodyBodyAccJerkMag-std"
+[75] | "fBodyBodyAccJerkMag-meanFreq"
+[76] | "fBodyBodyGyroMag-mean"
+[77] | "fBodyBodyGyroMag-std"
+[78] | "fBodyBodyGyroMag-meanFreq"
+[79] | "fBodyBodyGyroJerkMag-mean"
+[80] | "fBodyBodyGyroJerkMag-std"
+[81] | "fBodyBodyGyroJerkMag-meanFreq"
+[82] | "angle(tBodyAccMean,gravity)"
+[83] | "angle(tBodyAccJerkMean,gravityMean)"
+[84] | "angle(tBodyGyroMean,gravityMean)"
+[85] | "angle(tBodyGyroJerkMean,gravityMean)"
+[86] | "angle(X,gravityMean)"
+[87] | "angle(Y,gravityMean)"
+[88] | "angle(Z,gravityMean)"
+
+###Clean up the data
+Some changes were performed to clean up the data. The activity names originally had code numbers (1 to 6) attached to their names. These numbers were omitted from the names, and the names were formatted with first capital letter, underlines were also omitted. For example: the activity name "2 WALKING_UPSTAIRS" replaced with "WalkingUpstairs".
+Some minor changes were made to the variables names, omitting only the "()" from their names.
+
+The full description of the workflow to clean up the data and creating the tidy data file **Mean_ActivityDataBySmartphones.txt**, was executed by the script [run_analysis.R](https://github.com/anat-kedem/TidyDataActivityRecognitionUsingSmartphones/run_analysis.R) as described in the [README.md](https://github.com/anat-kedem/TidyDataActivityRecognitionUsingSmartphones/README.md) file.
+
+
---
+++ run_analysis.R
@@ -0,0 +1,99 @@
+run_analysis <- function() {
+##---------------------------- instructions --------------------------------------
+##You should create one R script called run_analysis.R that does the following. 
+##1. Merges the training and the test sets to create one data set.
+##2. Extracts only the measurements on the mean and standard deviation for each measurement
+##3. Uses descriptive activity names to name the activities in the data set
+##4. Appropriately labels the data set with descriptive variable names. 
+##5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+##--------------------------------------------------------------------------------
+
+        ##read data from working directory: originally some of the data is in sub-directories "test" and "train"
+        train.activity <- read.table("./train/y_train.txt", colClasses = "character")
+        test.activity <- read.table("./test/y_test.txt", colClasses = "character")
+        
+        train.subject <- read.table("./train/subject_train.txt", colClasses = "character")
+        test.subject <- read.table("./test/subject_test.txt", colClasses = "character")
+        
+        train.data <- read.table("./train/X_train.txt")
+        test.data <- read.table("./test/X_test.txt")
+        
+        features.names <- read.table("features.txt", colClasses = "character")
+        activity.names <- read.table("activity_labels.txt", colClasses = "character")
+        
+        ##set column names and build one dateset that contain both test and train data
+        ##1. build train dataset
+        colnames(train.data) <- features.names[,2] ##add the "features" as column names
+        colnames(train.activity) <- "activityName" ##add a column name for the activitiy column
+        colnames(train.subject) <- "subject" ##add a column name for the subject column
+        train <- cbind(train.subject, train.activity, train.data, deparse.level = 0) ##create data frame: column one is subject number, column two is activity number, other columns are the results data
+        
+        ##2. build test dataset
+        colnames(test.data) <- features.names[,2] ##add the features as column names
+        colnames(test.activity) <- "activityName" ##add a column name for the activitiy column
+        colnames(test.subject) <- "subject" ##add a column name for the subject column
+        test <- cbind(test.subject, test.activity, test.data, deparse.level = 0) ##create data frame: column one is subject number, column two is activity number, other columns are the results data
+        
+        ##3. merge into one dataset, and sort it
+        largeDataset <- rbind(train, test) ##add rows of train set and rows of test set, into new data frame
+        largeDataset <- largeDataset[order(largeDataset$subject,largeDataset$activityName),] ##sort the data frame
+        
+        
+        ##create smaller dataset that contain data for mean and std (and the subject and activity)
+        ##1. filter the large dataset into smaller dataset
+        colMeanStd <- grepl("[Mm]ean|std",names(largeDataset)) ##logical vector indicating which of the columns contain  one of the words: mean, Mean, or std
+        colMeanStd[1] <- TRUE ##update the first cell in the logical vector to keep also column "subjuect"
+        colMeanStd[2] <- TRUE ##update the second cell in the logical vector to keep slao column "activity"
+        smallDataset <- largeDataset[,colMeanStd] ##create the filtered data set using the logical vector
+        
+        ##2. edit activity names to descriptive names, remove some brackets from the variable names
+        edited.activity.names <- activity.names
+        edited.activity.names[,2] <- sub("WALKING", "Walking" ,edited.activity.names[,2])
+        edited.activity.names[,2] <- sub("_UPSTAIRS", "Upstairs" ,edited.activity.names[,2])
+        edited.activity.names[,2] <- sub("_DOWNSTAIRS", "Downstairs" ,edited.activity.names[,2])
+        edited.activity.names[,2] <- sub("SITTING", "Sitting" ,edited.activity.names[,2])
+        edited.activity.names[,2] <- sub("STANDING", "Standing" ,edited.activity.names[,2])
+        edited.activity.names[,2] <- sub("LAYING", "Laying" ,edited.activity.names[,2])
+        
+        for(i in activity.names[,1]) {
+                smallDataset$activityName <- sub(i, edited.activity.names[as.numeric(i),2], smallDataset$activityName)
+        }
+        names(smallDataset) <- sub("[(][)]", "", names(smallDataset))
+        names(smallDataset)[83] <- sub("[)]", "", names(smallDataset)[83])
+        
+        ##create the summary dataset: for each subject there is a mean for each activity and one mean for all activities
+        ##1. build data frame to store the means
+        subjectList <- unique(smallDataset$subject) ##basically, 1 to 30
+        subjectNum <- length(subjectList) ##how many subjects (30)
+        activityList <- unique(smallDataset$activityName) ##six activity names
+        activityNum <- length(activityList) ##how many activities (6)
+        variableNum <- ncol(smallDataset)
+        meanDataset <- data.frame(
+                Subject=unlist(lapply(c("1":"30"), function(x) {rep(x, (activityNum+1))})), ##for each subject 7 rows (6 rows activities + mean of all)
+                Activities=rep(c(activityList, "Mean_of_Activities"), subjectNum)
+        )
+        emptyMean <- data.frame(matrix(ncol = (ncol(smallDataset)-2), nrow = nrow(meanDataset))) ##create matrix with empty (NA) columns to store all variable means
+        colnames(emptyMean) <- colnames(smallDataset[3:ncol(smallDataset)]) ##names for the empty columns
+        meanDataset <- cbind(meanDataset, emptyMean, deparse.level = 0) ##data frame is ready to store mean values
+        
+        ##2. fill data frame with of means of variables
+        for(i in 1:210) {  ##pass through each row
+                ##for(j in 3:(ncol(smallDataset))) {  ##pass through each column
+                for(j in 3:88) {  ##pass through each column
+                        meanDataset[i,j] <- with(smallDataset, mean(smallDataset[,j][subject==meanDataset[i,1] & activityName==meanDataset[i,2]], na.rm=TRUE))
+                }
+        }
+        ##3. fill data frame with means of all activities together for each subject
+        for(i in seq(7, 210, by=7)) {
+                for(j in 3:88) {  ##pass through each column
+                        meanDataset[i,j] <- with(meanDataset, mean(meanDataset[,j][Subject==meanDataset[i,1]], na.rm=TRUE))
+                }
+        }
+        
+        
+        ##write the tidy data to a file
+        write.table(meanDataset, file="Mean_ActivityDataBySmartphones.txt", row.names=FALSE)
+
+##to read this file use the following:
+##backupSet <- read.table(file="Mean_ActivityDataBySmartphones.txt", colClasses=c("character", "character",rep("numeric",86)), header=TRUE)
+}
